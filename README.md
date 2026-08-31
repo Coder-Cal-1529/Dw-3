3b24413ff199bac35ed730fde78b87bc75926a8d
Coder-Cal-1529 <caleb.kellum@my.tccd.edu>
2026-08-31 09:41:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Debugging Warm-Up 03
+
+In this program, the user is prompted to enter a temperature in Fahrenheit and it outputs if the temperature is above or below freezing.
+
+## Flow Chart
+
+![flowchart](imgs/flowchart.png)
+
+## Pseudocode
+
+```
+FUNCTION main()
+    DECLARE temp : FLOAT
+
+    OUTPUT "Enter the temperature in Fahrenheit"
+    INPUT temp
+
+    IF temp greater than or equal to 32:
+        OUTPUT "Above freezing"
+    ELSE
+        OUTPUT "Below freezing"
+    ENDIF
+ENDFUNCTION
+```
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn main() {
+    // Declaring variables
+    let mut input = String::new();
+    let temp: f64;
+
+    // Getting user input for the temperature to check
+    println!("Enter a temperature in Fahrenheit");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    // Converting the input to a 64 bit float
+    temp = input.trim().parse().expect("Not a number");
+
+    // Checking the input value against the freezing point in fahrenheit
+    if temp <= 32.0 {
+        println!("Above freezing");
+    } else {
+        println!("Below freezing");
+    }
+}
+
